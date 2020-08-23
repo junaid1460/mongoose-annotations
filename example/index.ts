16444f72adcedb63d82f7edd75fb33d1d2c9636d
Junaid <junaid1460@gmail.com>
2020-08-23 20:00:54 +0530

    create example usage code

---
+++ example/index.ts
@@ -0,0 +1,42 @@
+@schema({ _id: false })
+class UserAuth {
+  @field() name?: string = "name";
+
+  @field() type?: number = 254;
+
+  getType() {
+    return this.type;
+  }
+}
+
+export class UserSchema {
+  @field()
+  name: string = "hello";
+
+  @field()
+  type: number = 23;
+
+  @field({ type: arrayOf(UserAuth) })
+  auth: UserAuth[] = [];
+
+  getName() {
+    return "helslls";
+  }
+
+  getMyName() {}
+}
+
+@collection("user", UserSchema)
+export class User extends MongooseModel<UserSchema>() {
+  static getAll() {
+    return this.find().exec();
+  }
+}
+
+User.findById("5f42181555791f7879cfaebc")
+  .exec()
+  .then((e) => {
+    e?.auth.push(new UserAuth());
+    e?.getMyName();
+    console.log(new User(e).name);
+  });
